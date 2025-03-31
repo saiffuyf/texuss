3ff1dd201e1a4bee8614c18934c43112244ccda4
saikiran601 <bommidenisaikiran423@gmail.com>
2025-03-31 18:37:22 +0530

    added static.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,5 @@ import { SignupComponent } from '../components/signup/signup.component';
 export const routes: Routes = [
     { path: '', redirectTo: 'signup', pathMatch: 'full' }, // Default route to signup
   { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: 'signup' }
 ];
